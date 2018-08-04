d609de787093058b72828a56663c1a814cf4001d
Dmitry Shatilov <shatilov.diman@gmail.com>
2018-08-04 23:08:33 +0300

    build: sync cmake build type

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ cd .build
 conan install ../conan/ -s compiler.version=7 -s arch=x86_64 -s build_type=Release --build=missing
 cmake ..
 cd ..
-cmake --build .build
+cmake --build .build --config Release
 
