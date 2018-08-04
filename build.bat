d23a69892a69ff2edb455776537024e22aebfe85
Dmitry Shatilov <shatilov.diman@gmail.com>
2018-08-04 22:59:40 +0300

    build: clean conan install

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 mkdir .build
 cd .build
-conan install ../conan/ -s compiler.version=15
+conan install ../conan/ -s compiler.version=15 -s arch=x86_64 -s build_type=Release --build=missing
 cmake -G "Visual Studio 15 2017 Win64" ..
 cd ..
 cmake --build .build --config Release
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir .build
 cd .build
-conan install ../conan/ -s compiler.version=7
+conan install ../conan/ -s compiler.version=7 -s arch=x86_64 -s build_type=Release --build=missing
 cmake ..
 cd ..
 cmake --build .build
