e752c8e29f84cfb33bc4951be747976eba66e868
Dmitry Shatilov <shatilov.diman@gmail.com>
2018-08-04 14:17:29 +0300

    Upload package conanfile

---
+++ conan/conanfile.py
@@ -0,0 +1,38 @@
+
+from conans import ConanFile, CMake, tools
+
+class LoggerConan(ConanFile):
+	name="loggerpp"
+	version="1.0"
+	description = "charivari's logger framework"
+	url = "https://github.com/shatilov-diman/loggerpp"
+	license = "GPL v3"
+
+	settings = "os", "compiler", "build_type", "arch"
+	options = {"shared": [True, False]}
+	default_options = "shared=False"
+	generators = "cmake"
+	requires = "gtest/master@conan/dev"
+
+	def configure(self):
+		self.settings.compiler["gcc"].version = "7"
+		self.settings.compiler["Visual Studio"].version = "15"
+
+	def source(self):
+		self.run("git clone https://github.com/shatilov-diman/loggerpp.git .")
+
+	def build(self):
+		cmake = CMake(self)
+		cmake.configure()
+		cmake.build()
+
+		self.run("./bin/tests")
+
+	def package(self):
+		self.copy("*.h")
+		self.copy("*.lib", dst="lib", keep_path=False)
+		self.copy("*.dll", dst="bin", keep_path=False)
+		self.copy("*.so", dst="lib", keep_path=False)
+		self.copy("*.dylib", dst="lib", keep_path=False)
+		self.copy("*.a", dst="lib", keep_path=False)
+
